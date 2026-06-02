d4f5b973cc2931a5f05c3868ebd6b9194a9cf130
Samuel242005 <samuka.aang@hotmail.com>
2026-06-02 14:42:57 -0300

    poções, ataque, turnos, escolha inicial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import database.DatabaseConnection;
 import java.sql.Connection;
+import java.util.Scanner;
+
 import database.DatabaseInitializer;
 import service.PokeApiService;
 import batalha.Batalha;
@@ -19,29 +21,77 @@ public class Main {
         //criar as tablas
         DatabaseInitializer.criarTabela();
 
-        Pokemon pikachu = new Pokemon(
-                25,
-                "Pikachu",
-                35,
-                55,
-                40,
-                "eletric",
-                5,
-                0
+        Pokemon jogador;
+
+        Pokemon bulbasaur =
+                new Pokemon(
+                        1,
+                        "Bulbasaur",
+                        45,
+                        49,
+                        49,
+                        "grass",
+                        5,
+                        0
+                );
+
+        Pokemon charmander =
+                new Pokemon(
+                        4,
+                        "Charmander",
+                        39,
+                        52,
+                        43,
+                        "fire",
+                        5,
+                        0
+                );
+
+        Pokemon squirtle =
+                new Pokemon(
+                        7,
+                        "Squirtle",
+                        44,
+                        48,
+                        65,
+                        "water",
+                        5,
+                        0
+                );
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Escolha seu Pokémon:");
+
+        System.out.println("1 - " + bulbasaur.getNome());
+        System.out.println("2 - " + charmander.getNome());
+        System.out.println("3 - " + squirtle.getNome());
+
+        int escolha = scanner.nextInt();
+
+        if (escolha == 1) {
+
+            jogador = bulbasaur;
+
+        }
+        else if (escolha == 2) {
+
+            jogador = charmander;
+
+        }
+        else {
+
+            jogador = squirtle;
+
+        }
+
+        System.out.println(
+                "Você escolheu "
+                        + jogador.getNome()
         );
 
-        Pokemon charmander = new Pokemon(
-                4,
-                "Charmander",
-                39,
-                52,
-                43,
-                "fire",
-                5,
-                0
-        );
 
-        Batalha.iniciar(pikachu, charmander);
+        Batalha.iniciar(jogador, charmander);
 
         PokeApiService.buscarPokemon();
     }
--- src/batalha/Batalha.java
+++ src/batalha/Batalha.java
@@ -3,41 +3,84 @@ import model.Pokemon;
 import java.util.Scanner;
 
 public class Batalha {
+
+
     public static void iniciar(Pokemon jogador, Pokemon inimigo)
     {
+        int pocoes = 5;
+        int turno = 1;
         System.out.println(jogador.getNome() + " VS " + inimigo.getNome());
         Scanner scanner = new Scanner(System.in);
         while (jogador.getHp() > 0 && inimigo.getHp() > 0)
         {
+            System.out.println("\n=== TURNO " + turno + " ====");
+            System.out.println(jogador.getNome() +" HP: "+ jogador.getHp() + "/" + jogador.getHpMax());
+            System.out.println(inimigo.getNome() + " HP: "+ inimigo.getHp() + "/" + inimigo.getHpMax());
+
+            System.out.println("Poções restantes: " + pocoes);
+
+
             System.out.println("\n1 - Atacar");
             System.out.println("2 - Usar Poção");
             System.out.println("3 - Fugir");
 
             int opcao = scanner.nextInt();
 
-            if (opcao == 1 ){
+            if (opcao == 1 )
+            {
                 atacar(jogador, inimigo);
-            } else if (opcao == 2) {
-                System.out.println("Pocão nao implementada");
-            } else {
-                System.out.println("Voce fugiu");
-                break;
             }
 
-            atacar(jogador, inimigo);
+            else if (opcao == 2) {
+                if (pocoes > 0) {
+                    jogador.setHp(Math.min(jogador.getHp() + 50, jogador.getHpMax()));
+                    pocoes--;
+                    System.out.println("Poção usada! HP atual: " + jogador.getHp() +"/"+ jogador.getHpMax());
+                    System.out.println("Poções restantes: " + pocoes);
+                    atacar(inimigo, jogador);
+
+                    turno++;
+                    continue;
+                } else {
+                    System.out.println("Você não tem possui mais poções!");
+                }
+            }
+
+            else if(opcao == 3){
+                System.out.println("Voce fugiu");
+                break;
+            }else{
+                System.out.println("opção invalida");
+                continue;
+            }
+
             if (inimigo.getHp() <= 0) {
                 System.out.println(inimigo.getNome() + " foi derrotado! ");
+
+                //sistema de ganho de xp
+                jogador.setXp(jogador.getXp() + 50);
+                if (jogador.getXp() >= 100){
+                    jogador.setLevel(jogador.getLevel() + 1);
+                    jogador.setXp(jogador.getXp() - 100);
+
+                    System.out.println(jogador.getNome() + " Subiu para o nivel "
+                                     + jogador.getLevel() + " !");
+                }
+                System.out.println(jogador.getNome() + " ganhou 50 XP !");
+                System.out.println("XP ATUAL: " + jogador.getXp());
+
                 break;
             }
 
             atacar(inimigo, jogador);
-
             if (jogador.getHp() <= 0) {
                 System.out.println(jogador.getNome() + " foi derrotado! ");
                 break;
             }
+            turno++;
         }
     }
+
 
         public static void atacar (Pokemon atacante, Pokemon alvo){
 
@@ -45,7 +88,7 @@ public class Batalha {
                 if (dano < 1) {
                     dano = 1;
                 }
-            alvo.setHp(alvo.getHp() - dano);
+            alvo.setHp(Math.max(0, alvo.getHp()-dano));
             System.out.println(atacante.getNome() + " causou " + dano + " de dano ");
 
             System.out.println(
